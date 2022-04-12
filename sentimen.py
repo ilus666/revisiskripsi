364fe8cf96315f55a56198bf891232f84afc2448
angelus dolf <pohonkelapa9e17@gmail.com>
2022-04-12 13:52:22 +0700

    first commit

---
+++ sentimen.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import joblib
+
+st.title('Prediksi Sentiment')
+st.subheader('Implementasi Sentiment Analysis Berdasarkan Tweets Masyarakat Terhadap Kinerja Presiden dalam Aspek Penanganan Covid-19')
+st.text('Algoritma SVM OneVSRest')
+
+#input
+my_form = st.form(key="form1")
+name = my_form.text_input(label = "Masukkan teks berbahasa indonesia:")
+submit = my_form.form_submit_button(label = 'submit')
+teks = name.title()
+
+#sistem
+model = joblib.load(open('modelB2.pkl', 'rb'))
+tfidf = joblib.load(open('tf_idfB2.pkl', 'rb'))
+data = tfidf.transform([teks])
+hasil = model.predict(data)
+hasil1 = " ".join(hasil)
+hasil_proba = model.predict_proba(data)
+
+if submit:
+     st.write('Hasil kelas emosinya adalah :',hasil1)
+
+
